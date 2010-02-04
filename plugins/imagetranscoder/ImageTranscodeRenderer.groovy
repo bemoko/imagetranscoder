bc8cbf9db0db4537f4375f64fe561b3037031aac
Ian Homer <ian.homer@bemoko.com>
2010-02-04 16:26:25 +0000

    First version of image transcoder plugin

---
+++ plugins/imagetranscoder/ImageTranscodeRenderer.groovy
@@ -0,0 +1,113 @@
+/**
+ * Copyright 2009 bemoko 
+ */
+ 
+/*
+ * Simple image transcoder
+ */
+package imagetranscoder
+
+import javax.imageio.ImageIO
+import javax.imageio.ImageWriteParam
+import javax.imageio.stream.FileImageOutputStream
+import javax.imageio.IIOImage
+
+import com.bemoko.live.platform.ui.Resource
+
+import com.bemoko.live.platform.mwc.plugins.AbstractRendererPlugin
+import com.bemoko.live.platform.ui.Resource
+
+class ImageTranscodeRenderer extends AbstractRendererPlugin {
+  def imageTranscoder=new ImageTranscoder()
+  def resource, resourcePath  
+  def cacheRoot
+  
+  void initialise(Map p) {
+    cacheRoot=p.cacheRoot ?: System.getProperty("java.io.tmpdir") + 
+      "/imagetranscoder/${site.name}"
+    log.info("Cache root : $cacheRoot")
+  }
+  
+  void execute(Map p) { 
+    try {
+      def resourcePath=getResourcePath(p)
+      if (resourcePath && new File(resourcePath).exists()) {
+        resource=new Resource(resourcePath,platform.contentTypeUtils
+          .getContentTypeFromPath(resourcePath))      
+      } else {
+        if (!resourcePath) {
+          log.error("Resource path of transcoded image is null")
+        } else {
+          log.error("Cannot find transcoded image path : $resourcePath")
+        }
+      }
+    } catch (e) {
+      log.error("Cannot deliver transcoded image ${p.key}; ${p.rule}; ${p.uri}",e)
+    }
+  }
+  
+  def getResourcePath(p) {
+    def key = p.uri.toLowerCase().replaceAll("[^a-z]","-")
+    def fileName="${cacheRoot}/${key}/${p.rule}"
+    
+    if (!new File(fileName).exists() || "true".equals(p.reload)) {
+      def imageOut=transcode(p.uri,p.rule,fileName)
+    }
+    return fileName
+  }
+
+  def transcode(uri,rule,fileName) {
+    def imageIn=getImage(uri)
+    def imageOut = imageTranscoder.rules(imageIn,rule)
+    if (imageOut) {
+      storeImage(imageOut,fileName)
+      return imageOut
+    }
+    throw new Exception("Unable to trancode : ${uri} ; ${rule} ; ${fileName}")
+  }
+  
+  def getImage(uri) {
+    def imageUrlAsString
+    if (uri.startsWith('/')) {
+      /*
+       * Local site image
+       */
+      imageUrlAsString=platform.intent.platformEndPoint + "/" +
+        platform.site.name + uri
+    } else {
+      imageUrlAsString=imageTranscoder.uncompressUri(uri)
+    }
+    try {
+      ImageIO.read(new URL(imageUrlAsString))
+    } catch (java.net.MalformedURLException e) {
+      throw new Exception("Can't read image from ${imageUrlAsString}",e)
+    }
+  }
+
+  /*
+   * If we need explicit control in storeImage we might want to do ...
+   */
+  def storeImage(image,fileName) {
+    log.info("Transcoding image saved to ${fileName}")  
+    def writers = ImageIO.getImageWritersByFormatName("jpeg")
+    def writer = writers.next()
+    def iwp = writer.getDefaultWriteParam()
+    iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT)
+    iwp.setCompressionQuality(0.8f)
+    def file=new File(fileName)
+    file.parentFile.mkdirs()
+    FileImageOutputStream output = new FileImageOutputStream(file);
+    writer.setOutput(output);
+    IIOImage iioImage = new IIOImage(image, null, null)
+    writer.write(null, iioImage, iwp)
+    writer.dispose()
+  }
+  
+  void close() {
+    if (resource) resource.close()
+  }
+
+  String getContentType() { resource?.contentType }
+  long getContentLength() { resource ? resource.length : 0 }
+  InputStream getInputStream() { resource?.inputStream }  
+}
---
+++ plugins/imagetranscoder/ImageTranscoder.groovy
@@ -0,0 +1,251 @@
+/**
+ * Copyright 2009 bemoko 
+ */
+package imagetranscoder
+
+import com.bemoko.live.platform.mwc.plugins.AbstractPlugin
+
+import java.awt.geom.AffineTransform
+import java.awt.image.AffineTransformOp
+import java.awt.Graphics2D
+import java.awt.image.BufferedImage
+import java.awt.Transparency
+import java.awt.RenderingHints
+import java.awt.image.ConvolveOp
+import java.awt.image.Kernel
+
+/*
+ * Image Transcoder
+ */
+class ImageTranscoder extends AbstractPlugin {  
+
+  /*
+   * Progressive downscaling is slower but better.  This comes into play
+   * if downscaling by more than 50%
+   */
+  private static PROGRESSIVE_DOWNSCALING=true
+  private static PROGRESSIVE_DOWNSCALING_FACTOR=2
+  private static COMPRESSION_PREPEND_KEY="b-"
+  
+  /*
+   * URI compression rules to shorten URLs.
+   */
+  def uriCompressionRules =  [
+     "http":"http://"
+   ]
+      
+  def rules = { image, rule ->
+    switch (rule) {
+      // Scale to width rule, e.g. "100.jpeg"
+      case ~/[0-9]*\.jpeg/ :
+        def m = rule =~ /([0-9]*)\.jpeg/
+        return scaleToSize(image,m[0][1] as int)        
+      // Scale to size rule, e.g. "120x60.jpeg"
+      case ~/[0-9]*x[0-9]*\.jpeg/ :
+        def m = rule =~ /([0-9]*)x([0-9]*)\.jpeg/
+        return scaleToSize(image,m[0][1] as int,m[0][2] as int)        
+      // Crop to width rule, e.g. "crop100.jpeg"
+      case ~/crop[0-9]*\.jpeg/ :
+        def m = rule =~ /crop([0-9]*)\.jpeg/
+        return cropToSize(image,m[0][1] as int)        
+      // Resize to width and then crop to width rule, e.g. "150-crop100.jpeg"
+      case ~/[0-9]*-crop[0-9]*\.jpeg/ :
+        def m = rule =~ /([0-9]*)-crop([0-9]*)\.jpeg/
+        return cropToSize(scaleToSize(image,m[0][1] as int),m[0][2] as int)
+      default :
+        throw new Exception("Rule $rule not recognised")
+    }  
+  }
+
+
+
+  /* 
+   * Get transcoded image uri for a given image input uri
+   */
+  def uri(rule,uri) {
+    "/t/rule/${rule}?uri=${compressUri(uri)}" + 
+      (platform.intent.reload ? "&reload=${platform.intent.reload}" : "")
+  }
+  
+  /*
+   * Not recommended as a scaling, but included as basic example
+   */
+  def scaleNearestNeighbor(imageIn,factor) {
+    def transform = new AffineTransform()
+    transform.scale(factor,factor)
+    def op = new AffineTransformOp(transform,AffineTransformOp.TYPE_NEAREST_NEIGHBOR )
+    def imageOut=op.filter(imageIn,null)  
+  }
+  
+  static scaleByFactor(imageIn,factor) {
+    return scaleToSize(imageIn,imageIn.width * factor,imageIn.height * factor)
+  }
+
+  /*
+   * Scale to size always maintaining aspect ratio and never up sizing
+   * 
+   * Multi step bilinear scaling as recommended at
+   * http://today.java.net/pub/a/today/2007/04/03/perils-of-image-getscaledinstance.html
+   */
+  def scaleToSize(imageIn,int targetWidth,int targetHeight=-1,graphicsCallback=null) {
+    int type = (imageIn.getTransparency() == Transparency.OPAQUE) ?
+      BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+    def (width,height) = [imageIn.width,imageIn.height]
+    if (targetWidth > width) targetWidth=width
+    if (targetHeight > height) targetHeight=height    
+    def imageOut=drawImage(imageIn,width,height,width,height,type,null)
+    int nextInputWidth, nextInputHeight, nextTargetWidth, nextTargetHeight    
+    nextInputWidth=width
+    nextInputHeight=height
+    while (true) {
+      boolean finalRendering=false
+      if (PROGRESSIVE_DOWNSCALING && 
+          (targetWidth < nextInputWidth/PROGRESSIVE_DOWNSCALING_FACTOR)) {
+        nextTargetWidth=nextInputWidth/PROGRESSIVE_DOWNSCALING_FACTOR
+        nextTargetHeight=nextInputHeight/PROGRESSIVE_DOWNSCALING_FACTOR
+      } else {
+        finalRendering=true
+        nextTargetWidth=targetWidth
+        nextTargetHeight=targetHeight        
+      }      
+      imageOut=drawImage(imageOut,nextTargetWidth,nextTargetHeight,
+        nextInputWidth,nextInputHeight,type,graphicsCallback,finalRendering)
+      if (finalRendering) {
+        return imageOut
+      } else {
+        nextInputWidth=nextTargetWidth
+        nextInputHeight=nextTargetHeight
+      }
+    }
+  }
+  
+  def drawImage(imageIn,int targetWidth,int targetHeight,
+      int originalWidth,int originalHeight,type,graphicsCallback,finalRendering=false) {
+    /*
+     * If target height is less than 0 then we want height calculated from width scaled by 
+     * aspect ratio
+     */
+    if (targetHeight == -1) {
+      targetHeight=originalHeight * targetWidth / originalWidth
+    }
+    /*
+     * Determine uncropped width and height
+     */
+    int uncroppedWidth,uncroppedHeight
+    if ( (targetWidth/targetHeight) > (originalWidth/originalHeight) ) {
+      /*
+       * Target image is more landscape => vertical cropping
+       */
+       uncroppedWidth = targetWidth
+       uncroppedHeight = targetWidth * originalHeight / originalWidth
+    } else {
+      /*
+       * Target image is more portrait => horizontal cropping
+       */
+       uncroppedWidth = targetHeight * originalWidth / originalHeight
+       uncroppedHeight = targetHeight
+    }
+    BufferedImage imageOut = new BufferedImage(targetWidth, targetHeight, type)
+    Graphics2D g2 = imageOut.createGraphics()    
+    try {
+      g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+          RenderingHints.VALUE_INTERPOLATION_BICUBIC)
+      if (true) {
+        g2.setRenderingHint(RenderingHints.KEY_RENDERING,
+            RenderingHints.VALUE_RENDER_QUALITY);            
+        g2.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING,
+            RenderingHints.VALUE_COLOR_RENDER_QUALITY)
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+            RenderingHints.VALUE_ANTIALIAS_OFF);
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+            RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);              
+        g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION,
+            RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY)
+      }
+      g2.drawImage(imageIn, 0, 0, uncroppedWidth,uncroppedHeight as int, null)
+      /*
+       * Allow the transcoding rule to post process image, e.g. overlay
+       */
+      if (finalRendering && graphicsCallback) {
+        graphicsCallback(g2)
+      }
+    } finally {
+      g2.dispose()
+    }
+    return imageOut
+  }
+  
+  def cropToSize(imageIn,int targetWidth) {
+    int width=imageIn.width
+    if (width > targetWidth) {
+      int margin = (width - targetWidth) / 2
+      return crop(imageIn,margin,0,targetWidth)
+    } else {
+      return imageIn
+    }    
+  }
+  
+  def crop(imageIn,int x, int y, int targetWidth=-1,int targetHeight=-1) {
+    int width=targetWidth > 0 ? targetWidth : (imageIn.width - x)
+    int height=targetHeight > 0 ? targetHeight : (imageIn.height - y)    
+    BufferedImage imageOut = new BufferedImage(width, height, getType(imageIn))
+    Graphics2D g2 = imageOut.createGraphics()  
+    g2.drawImage(imageIn, -x, -y, imageIn.width,imageIn.height, null)  
+    return imageOut
+  }
+  
+  def getType(image) {
+    (image.getTransparency() == Transparency.OPAQUE) ?
+      BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB
+  }
+  
+  /**
+   * Blur an image
+   */
+  def blur(imageIn) {
+    ConvolveOp convolve = new ConvolveOp(blurKernel, ConvolveOp.EDGE_NO_OP,null);
+    def imageOut=new BufferedImage(imageIn.width, imageIn.height, getType(imageIn))
+    convolve.filter(imageIn, imageOut);
+    return imageOut
+  }
+  
+  /**
+   * Blur kernel to define how blur should be applied
+   */
+  @Lazy blurKernel = {
+    def middle=0.1
+    def edge=0.05
+    def corner=(1-middle-4*edge)/4.0
+    L.debug("$middle : $edge : $corner")
+    
+    new Kernel(3, 3, 
+      [ corner, edge, corner, edge, middle, edge, corner, middle, corner ] as float[]);    
+  }()
+  
+  /*
+   * Compress a URI
+   */
+  def compressUri(uri) {
+    def compressedUri=uri
+    for (compressionRule in uriCompressionRules) {
+      if (compressedUri.startsWith(compressionRule.value)) {
+        compressedUri = COMPRESSION_PREPEND_KEY + compressionRule.key + "-" +    
+          compressedUri.substring(compressionRule.value.length())
+        break
+      }
+    }
+    return compressedUri
+  }
+  
+  
+  /*
+   * Uncompress a URI
+   */
+  def uncompressUri(uri) {
+    def uncompressedUri=uri
+    def m = (uri =~ /^b-([^-]*)-(.*)/)
+    if (m.matches()) {
+      uncompressedUri=uriCompressionRules[m[0][1]] + m[0][2]
+    }
+  }  
+}
